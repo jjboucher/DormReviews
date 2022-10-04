1c57b8f9fc87eb1ac2eb1e6262d16cd4608727ca
Joshua Boucher <jjbouche@syr.edu>
2022-10-04 11:26:14 -0400

    Created classes and DB schema

--- dormapp/models.py
+++ dormapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class University(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+
+class ResHall(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+
+class DormRoom(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    roomNumber = models.CharField(max_length=255)
+    resHall = models.ForeignKey(ResHall, on_delete=models.CASCADE)
+
+class ResHallReview(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    resHall = models.ForeignKey(ResHall, on_delete=models.CASCADE)
+    starRating = models.IntegerField(default=0)
+    reviewTitle = models.CharField(max_length=255)
+    reviewBody = models.TextField(max_length=1500)
+
+class DormRoomReview(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    dormRoom = models.ForeignKey(DormRoom, on_delete=models.CASCADE)
+    starRating = models.IntegerField(default=0)
+    reviewTitle = models.CharField(max_length=255)
+    reviewBody = models.TextField(max_length=1500)
+
+class ResHallPhoto(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    resHall = models.ForeignKey(ResHall, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='res-hall-photos')
+
+class DormRoomPhoto(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    dormRoom = models.ForeignKey(ResHall, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='dorm-room-photos')
