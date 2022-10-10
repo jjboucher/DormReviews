7b38770ef6eec9df6f88234bff2d6c5dec119a1c
Joshua Boucher <jjbouche@syr.edu>
2022-10-10 16:53:51 -0400

    created dorm room query

--- dormapp/helpers/queries.py
+++ dormapp/helpers/queries.py
@@ -1,4 +1,4 @@
-from dormapp.models import models as m
+import dormapp.models as m
 
 def getUniversities():
     universities = m.University.objects.all()
@@ -24,5 +24,25 @@ def getResHalls(universityId):
         thumbnail = m.ResHallPhoto.filter(resHall = resHall.id).latest('dateCreated').photo
 
         returnList.append(resHallView(resHall.name, thumbnail, averageRating))
+    
+    return returnList
+
+class dormRoomView():
+    def __init__(self, name, thumbnail, rating):
+        self.name = name
+        self.thumbnail = thumbnail
+        self.rating = rating
+
+def getDormRooms(resHallId):
+    dormRooms = m.DormRoom.filter(resHall = resHallId)
+    returnList = []
+    for dormRoom in dormRooms:
+        ratings = m.DormRoomReview.filter(dormRoom = dormRoom.id).values('starRating')
+    
+        averageRating = sum(ratings) / len(ratings)
+
+        thumbnail = m.DormRoomPhoto.filter(dormRoom = dormRoom.Id).latest('dateCreated').photo
+
+        returnList.append(dormRoomView(dormRoom.name, thumbnail, averageRating))
     
     return returnList
--- dormapp/views.py
+++ dormapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .helpers import queries
+from dormapp.helpers import queries
 
 # Create your views here.
 
