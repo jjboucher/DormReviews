b6a953cc19db242cc0b29940233a3cf0221856b3
andyLiSyr <ali142@syr.edu>
2022-10-12 15:19:25 -0400

    Edited resHalls templates

--- dormapp/admin.py
+++ dormapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import University
+from .models import University,ResHall,ResHallReview
 # Register your models here.
 
 admin.site.register(University)
+admin.site.register(ResHall)
+admin.site.register(ResHallReview)
